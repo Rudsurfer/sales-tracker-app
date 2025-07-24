0829979f8228f2fef7e9b24cbb257a3e8c19a335
Rudsurfer <yves.a@rudsak.com>
2025-07-24 17:12:12 -0400

    Add files via upload

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBHbryV3zYF2-J00mift241A_sUBRtKSl4",
+  authDomain: "rudsak-sales-payroll-tra-2c94b.firebaseapp.com",
+  projectId: "rudsak-sales-payroll-tra-2c94b",
+  storageBucket: "rudsak-sales-payroll-tra-2c94b.appspot.com",
+  messagingSenderId: "261701010314",
+  appId: "1:261701010314:web:834be29dc22a5f6aa03e73"
+};
+
+export const appId = 'rudsak-sales-payroll-app';
