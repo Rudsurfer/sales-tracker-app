667e708c0f52b48ffc9ac19cc34579bc4646739a
Rudsurfer <yves.a@rudsak.com>
2025-10-03 09:29:46 -0400

    Update Schedule.jsx

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -236,7 +236,7 @@ export const Schedule = ({ allEmployees, selectedStore, currentWeek, currentYear
                                                 <td className="px-4 py-2 text-center font-bold">{decimalHoursToHM(totalScheduledHours)}</td>
                                                 <td className="px-4 py-2 text-center font-bold print-hide">{decimalHoursToHM(totalActualHours)}</td>
                                                 <td className="px-4 py-2 text-center no-print">
-                                                    <button onClick={() => handleRemoveRow(row.EmployeeID)} className="text-red-500 hover:text-red-400"><Trash2 size={1T8} /></button>
+                                                    <button onClick={() => handleRemoveRow(row.EmployeeID)} className="text-red-500 hover:text-red-400"><Trash2 size={18} /></button>
                                                 </td>
                                             </tr>
                                         )
@@ -270,3 +270,4 @@ export const Schedule = ({ allEmployees, selectedStore, currentWeek, currentYear
         </>
     );
 };
+
